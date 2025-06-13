8f61c1ae76a7b4312867b3db7153db9dfcfd5acd
Toffee <160152673+toffeecaramel@users.noreply.github.com>
2025-06-13 18:38:45 -0300

    more progress on the Typer.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,6 +1,7 @@
 package;
 
 import DialogueBox.Positioning;
+import backend.utils.*;
 import flixel.FlxG;
 import flixel.FlxState;
 import flixel.addons.effects.FlxTrail;
@@ -11,36 +12,22 @@ import game.chars.*;
 
 class PlayState extends FlxState
 {
-	private var box:DialogueBox;
-	final datas:Array<Dynamic> = 
-	[
-		['default', null, null, '* You decide to kill\nyourself rn', BOTTOM],
-		['darkworld', null, null, '* You decide to kill\nyourself but in the\ndark world, wow.', TOP],
-		['default', 'susie', 'confused', '* Kris... WHERE THE\nFUCK ARE WE?!', BOTTOM],
-		['darkworld', 'noelle', 'angry', '* ...I hate you.', CENTER]
-	];
-
 	var bg = new Background();
-	var krisGhost:FlxTrail;
 
 	override public function create()
 	{
 		super.create();
-		
-		add(bg);
-		box = new DialogueBox();
-		add(box);
+		var thing = new Typer();
+		thing.paused = false;
+		thing.onType.add((index, letter, text) -> 
+		{
+			trace('\nindex: $index\nletter: $letter\ntext: $text');
+		});
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
-		if(FlxG.keys.justPressed.O)
-		{
-			final data:Array<Dynamic> = datas[FlxG.random.int(0, datas.length - 1)];
-			box.show(data[0], data[1], data[2], data[3], data[4]);
-			trace(data);
-		}
 	}
 
 	public function startBattle()
--- source/backend/utils/Typer.hx
+++ source/backend/utils/Typer.hx
@@ -1,34 +1,53 @@
 package backend.utils;
 
+import flixel.FlxG;
 import flixel.util.FlxSignal;
 
 using StringTools;
 
-// Umm nvm this I'll move it to somewhere else
-/*typedef DialogData = {
-    var speed:Float;
-    var separatorsPause:Bool;
-    var text
-}*/
-
-@:publicFields //I am addicted to using public fields, help
-class Typer
-{
-    // -- Public variables (variables that can be changed/accessed from outside) -- //
-
+typedef TyperStruct = {
     /**
      * The text that will be displayed once typing.
-     * To start the typer, call `start()`
      */
-    var text:String = 'Hello, Dark World!';
+    var ?text:String;
 
     /**
      * The speed in which the text will type.
      */
-    var speed:Float = 0.04;
+    var ?speed:Float;
 
     /**
-     * The current letter index.
+     * Whether should it start with a delay.
+     */
+    var ?startDelay:Float;
+
+    /**
+     * Whether the typer should do a small pause when there's characters such as ",", ".", "!", "?", etc...
+     */
+    var ?separatorsPause:Bool;
+}
+
+@:publicFields //I am addicted to using public fields, help
+
+/**
+ * A class for helping out on typed-like texts.
+ */
+class Typer
+{
+    // -- Public variables (variables that can be changed/accessed from outside) -- //
+    
+    /**
+     * All the typer parameters, such as text, speed, etc.
+     */
+    var parameters:TyperStruct;
+
+    /**
+     * Whether the typer is paused or not.
+     */
+    var paused:Bool = true;
+
+    /**
+     * The last displayed letter's index.
      */
     var curIndex:Int = 0;
 
@@ -36,6 +55,11 @@ class Typer
      * The last displayed letter.
      */
     var curLetter:String = '';
+
+    /**
+     * The last displayed text.
+     */
+    var curText:String = '';
 
     /**
      * Signal dispatched when the typing starts.
@@ -51,13 +75,51 @@ class Typer
      * Signal dispatched when the text finishes typing.
      */
     final onFinish = new FlxTypedSignal<Void->Void>();
-
-    //TODO: an array for keys that skips the text to when it finishes.
     
     // -- Private variables (Only can be accessed by this class, unless @:privateAccess) -- //
 
     /**
-     * Whether the typer is paused or not.
+     * A timer used for every character.
      */
-    private var paused:Bool = true;
+    private var _timer:Float = 0;
+
+    /**
+     * The characters in which will pause the text for a brief time. Only works if `separatorsPause` is true.
+     */
+    private final separators:Array<String> = [
+        ',', '.', ';', ':', '!', '?' //TODO: maybe add more to this? 
+    ];
+
+    // -- Regular functions -- //
+
+    function new(?parameters:TyperStruct)
+    {
+        this.parameters = parameters ?? {
+            text: 'Hello, Dark World!',
+            speed: 0.04,
+            startDelay: 0,
+            separatorsPause: true
+        };
+
+        FlxG.signals.postUpdate.add(update);
+    }
+
+    function update()
+    {
+        if (paused || parameters.text == null || curIndex >= parameters.text.length) return;
+
+        _timer += FlxG.elapsed;
+        if (_timer >= parameters.speed)
+        {
+            _timer = 0;
+            curLetter = parameters.text.charAt(curIndex++);
+            curText += curLetter;
+            onType.dispatch(curIndex, curLetter, curText);
+
+            if (curIndex >= parameters.text.length)
+                onFinish.dispatch();
+        }
+    }
+
+    // -- Get/Setters Functions (nothing so far) -- //
 }
